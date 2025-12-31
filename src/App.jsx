fcf9f02b0c99d3a1f7281fbe98cf4e1a677d6b9e
Tirth02 <pateltirth0902@gmail.com>
2025-12-31 09:33:05 -0500

    Project setup with integration of Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-yellow-200 text-green-700'>Hello Project</h1>
+    </>
+  )
+}
+
+export default App
